f717f48db8dbaa070f5a6ac990300254a6200468
ricky3997 <riccardo@broggi.co.uk>
2017-07-01 15:34:17 +0100

    Split activities to work on different stuff

---
+++ app/src/main/java/it/centotrenta/expridge/AddItems.java
@@ -0,0 +1,13 @@
+package it.centotrenta.expridge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_items);
+    }
+}
